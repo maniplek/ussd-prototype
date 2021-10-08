e247c9872f40ceb610262166660a5cb6b95b95ad
Maniple Kwizera <83705949+maniplek@users.noreply.github.com>
2021-10-08 17:08:18 +0200

    mongo connection

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const ussdRoute = require('./index');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+
+// KQw0n000qV6ig7Dh
+
+
 const app = express();
 
 //model 
@@ -10,24 +14,7 @@ const user = require('./model/user');
 //const bank = require('./model/banks');
 
 //mongo connection 
-// const database_url = 'mongodb://localhost:27017/job-ussd';
-// mongoose.connect(database_url);
-// const db = mongoose.connection;
-// db.on('error',(err)=>{
-//  console.log(err);
-// });
-// db.once('open',()=>{
-//     console.log('Database is running...');
-// });
 
-module.exports = function(){
-    mongoose.connect("mongodb+srv://admin:admin@cluster0.a9jkr.mongodb.net/admin?retryWrites=true&w=majority",
-                    { useNewUrlParser: true, useUnifiedTopology: true})
-            .then(()=> console.log('Connected to mongodb.....')).catch((e) => {
-                console.log('Unexpected error occured while connecting to mongodb', e);
-            });
-          
-    }
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
---
+++ keys.js
@@ -0,0 +1,3 @@
+module.exports={
+    MONGOURI:"mongodb+srv://admin:<password>@cluster0.nsdwm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+}
